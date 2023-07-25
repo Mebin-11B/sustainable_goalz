b5f44161af61715f567e763b5419475536e36438
mebin <39584840+Mebin-Thattil@users.noreply.github.com>
2023-07-25 18:49:32 +0530

    displaying time also

--- main/main.py
+++ main/main.py
@@ -1,9 +1,11 @@
 from flask import *
+from sahana.timee import cur_t
 app = Flask(__name__)
 
 @app.route('/')
 def message():
-      return render_template("index.html")
+    t = cur_t()
+    return render_template("index.html",t = t)
 
 if __name__ == '__main__':
    app.run(debug = True)
