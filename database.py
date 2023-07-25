bb390da2cde8ffc4a9afdc4070b6184b4a7e5cc4
sahana-athota <sahana.athota@gmail.com>
2023-07-25 18:09:37 +0530

    database structure added

--- database.py
+++ database.py
@@ -17,7 +17,10 @@ for link in soup.find_all('a',
                           attrs={'href': re.compile("^https://")}):
     # display the actual urls
     print(link.get('href'))  
+
 '''
+
+
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -27,12 +30,33 @@ mydb = mysql.connector.connect(
 
 mycursor=mydb.cursor()
 
+def initializing():
+  mycursor.execute("create database if not exists hospitals ;")
+  mycursor.execute("use hospitals;")
+  mycursor.execute("create table hos (name varchar(500),lat varchar(100),lon varchar(100));")
+  mycursor.execute("insert into hos values('manipal hospital bangalore','12.958808','77.649141');")
+  mycursor.execute("insert into hos values('Victoria hospital','12.9580295012','77.5709560495');")
+  mycursor.execute("insert into hos values('ESI Hospital','12.9900215','77.55323450000003');")
+  mycursor.execute("insert into hos values('Banglore Hospital','13.006752','77.561737');")
+  mycursor.execute("insert into hos values('airforce hostpital','12.95801','77.63901');")
 
-mycursor.execute("create database if not exists hospitals ;")
-mycursor.execute("use hospitals;")
-mycursor.execute("create table hos (name varchar(500),lat varchar(100),lon varchar(100));")
-mycursor.execute("insert into hos values('manipal hospital bangalore','12.958808','77.649141');")
-mycursor.execute("insert into hos values('Victoria hospital','12.9580295012','77.5709560495');")
-mycursor.execute("insert into hos values('ESI Hospital','12.9900215','77.55323450000003');")
-mycursor.execute("insert into hos values('Banglore Hospital','13.006752','77.561737');")
-mycursor.execute("insert into hos values('airforce hostpital','12.95801','77.63901');")
+def values():
+    mycursor.execute("use hospitals;")
+    names=[]
+    mycursor.execute("select name from hos;")
+    o=mycursor.fetchall()
+    for i in o:
+        names.append(i[0])
+    lat=[]
+    mycursor.execute("select lat from hos;")
+    o=mycursor.fetchall()
+    for i in o:
+        lat.append(i[0])
+    lon=[]
+    mycursor.execute("select lon from hos;")
+    o=mycursor.fetchall()
+    for i in o:
+        lon.append(i[0])
+    return names,lat,lon
+
+
